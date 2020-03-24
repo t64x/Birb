99d0964fe5fa327c7b67b05eb3de4a5281f2cdd7
t64x <48433089+t64x@users.noreply.github.com>
2020-03-24 13:14:36 +0700

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-message.channel.send(`For commands/cmds click here!-><a title="commands" href="https://docs.google.com/document/d/1i-GjLmnSgSXQ7yCPAy5ATtflUxO-VLOsl8v94IS_s8s/edit?usp=sharing">commands</a>`);
+message.channel.send(`For commands/cmds click here!-><a href="https://docs.google.com/document/d/1i-GjLmnSgSXQ7yCPAy5ATtflUxO-VLOsl8v94IS_s8s/edit?usp=sharing%22%3Ecommands" target="_blank">click here</a>`);
 
 }
 module.exports.help = {
