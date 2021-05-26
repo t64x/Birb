5ece2bfcecc72d8efc916fa893c0f0602be82063
t64x <48433089+t64x@users.noreply.github.com>
2021-05-26 18:04:07 +0700

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-message.channel.send(`For commands/cmds click here!-><a href="https://docs.google.com/document/d/1i-GjLmnSgSXQ7yCPAy5ATtflUxO-VLOsl8v94IS_s8s/edit?usp=sharing%22%3Ecommands" target="_blank">click here</a>`);
+message.channel.send(`rn i have 2 commands, "ping" and "amogus <- this shit retard"`);
 
 }
 module.exports.help = {
